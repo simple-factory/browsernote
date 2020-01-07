6828ad74ede102018663f2c1725ceae107dd326a
honest.k <honest.k@kakaoenterpirse.com>
2020-01-07 09:45:00 +0900

    Updates

---
+++ precache-manifest.0c12e8a8e088905e7c860d7f4d08c85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f096e59859bebfdada05648112f7c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "060afbc446c0eceaecc0",
+    "url": "/static/css/main.ad277072.chunk.css"
+  },
+  {
+    "revision": "1f963e51599b4a8a598f",
+    "url": "/static/js/2.77d0cfcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.77d0cfcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "060afbc446c0eceaecc0",
+    "url": "/static/js/main.9bd720ad.chunk.js"
+  },
+  {
+    "revision": "fffdd8130dc5ab2269fb",
+    "url": "/static/js/runtime-main.5a9b2b0b.js"
+  }
+]);

707b6460add2bf97071eeced7e063f62133e17a4
kjy2143 <kjy2143@gmail.com>
2020-01-19 22:00:26 +0900

    Updates

---
+++ precache-manifest.e157ecbd0baaee3b471092c7bc6c7752.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c226276b306039eaf69399a47ecbda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62de55d49c3aacd2a691",
+    "url": "/static/css/2.0cc37800.chunk.css"
+  },
+  {
+    "revision": "4dda31a7888b185f383c",
+    "url": "/static/css/main.92d8f359.chunk.css"
+  },
+  {
+    "revision": "62de55d49c3aacd2a691",
+    "url": "/static/js/2.7e7294cb.chunk.js"
+  },
+  {
+    "revision": "ff7c0c75ea6356613a9e4fcc31df61cd",
+    "url": "/static/js/2.7e7294cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dda31a7888b185f383c",
+    "url": "/static/js/main.f23e5c6f.chunk.js"
+  },
+  {
+    "revision": "52e69851850f6747cc9c",
+    "url": "/static/js/runtime-main.9923b166.js"
+  },
+  {
+    "revision": "b864b19ef384262b069f7d4172652fc6",
+    "url": "/static/media/logo.b864b19e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b31c9bc1856f32bd86384ebddedf455.js"
+  "/precache-manifest.e157ecbd0baaee3b471092c7bc6c7752.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
